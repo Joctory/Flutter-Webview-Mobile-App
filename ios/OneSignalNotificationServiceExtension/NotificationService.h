a0a5ee3d210dbcac7620f1b571220ad2bc713a42
Joel Lee Min Kang <61981192+Joctory@users.noreply.github.com>
2023-05-12 11:54:55 +0800

    Add files via upload

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Joel Lee on 02/04/2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
